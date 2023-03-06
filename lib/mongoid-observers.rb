e5392774886004f715c93d129845da42c8205eb7
Michael Baldry <michael@brightbits.co.uk>
2023-03-06 11:52:36 +0000

    fix

--- lib/mongoid-observers.rb
+++ lib/mongoid-observers.rb
@@ -6,13 +6,15 @@ require "mongoid/observers/config"
 require "mongoid/observers/interceptable"
 require "mongoid/observers/railtie" if defined? Rails
 require "mongoid/observer"
+require "forwardable"
 
 module Mongoid
+  extend Forwardable
   include ActiveModel::Observing
 
   # Use def_delegators from Forwardable instead of delegate from ActiveSupport as it conflicts
   # with Forwardable delegate/instance_delegate.
   # See: https://jira.mongodb.org/browse/MONGOID-4849
 
-  def_delegators Config, *(ActiveModel::Observing::ClassMethods.public_instance_methods(false))
+  def_delegators Config, *ActiveModel::Observing::ClassMethods.public_instance_methods(false)
 end
